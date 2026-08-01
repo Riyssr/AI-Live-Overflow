dedac78b291044e1738b412e8906b1115c38263c
Riyssr <2833417024@qq.com>
2026-08-01 19:26:08 +0800

    init: Android project build.gradle

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+// Top-level build file for Devity DeskPet
+// Android悬浮窗 AI 陪伴桌宠
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+}
