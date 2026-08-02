884024fbc5db552e5398310b1466da3f76e9bcbf
Riyssr <2833417024@qq.com>
2026-08-02 19:24:33 +0800

    fix: add appcompat 1.6.1 (styles/AppCompatActivity need Theme.AppCompat)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,4 +52,5 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.webkit:webkit:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
 }
