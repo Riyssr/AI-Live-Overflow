17ff0c6c363d26bdf7570f9ed6fd22fc4fe541eb
Riyssr <2833417024@qq.com>
2026-08-02 18:10:17 +0800

    fix: bump AGP to 8.5.2 for Kotlin 2.4.0 compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file for Devity DeskPet
 plugins {
-    id("com.android.application") version "8.5.0" apply false
+    id("com.android.application") version "8.5.2" apply false
     id("org.jetbrains.kotlin.android") version "2.4.0" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.4.0" apply false
 }
